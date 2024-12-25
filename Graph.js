00c89db5d61b8f8c232c347b54232fa5bbc5d027
AGX <agxnh2020@gmail.com>
2024-12-26 00:43:19 +0200

    finished Task 7 Putting the data into a graph after fixing a bug

--- Graph.js
+++ Graph.js
@@ -1,16 +1,19 @@
-export default function getGraph(obj) {
-  const users = obj[users][user]; // return the array of users
-  // adjaceny list
+export function getGraph(obj) {
+  const users = obj.users[0].user; // Access the array of users correctly
   const graph = {};
 
   for (let i = 0; i < users.length; i++) {
     const user = users[i];
     const id = user.id;
-    const name = user.name;
     const followers = user.followers.follower;
 
-    graph[id] = followers.map((follower) => follower.id);
+    // Handle cases where there's only one follower or multiple followers
+    const followerArray = Array.isArray(followers) ? followers : [followers];
+
+    graph[id] = followerArray.map((follower) => follower.id);
   }
 
   return graph;
 }
+
+// { '1': [ '2', '3' ], '2': [ '1' ], '3': [ '1' ] }
